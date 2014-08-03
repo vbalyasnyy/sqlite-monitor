a13efffd3276df3c95e3777c3b02f386258ec279
vbalyasnyy <vsbal@mail.ru>
2014-08-03 18:12:44 +0400

    Add simple script test.

--- test_sqlite_build.sh
+++ test_sqlite_build.sh
@@ -1,13 +1,22 @@
 SQLITE_VERSION=3080500
 
-wget www.sqlite.org/2014/sqlite-autoconf-${SQLITE_VERSION}.tar.gz
+echo ">>> GET SQLITE SOURCES ..."
+ls ./sqlite-autoconf-${SQLITE_VERSION}.tar.gz
+if [ "x$?" != "x0" ];
+then
+	wget www.sqlite.org/2014/sqlite-autoconf-${SQLITE_VERSION}.tar.gz
+fi
+rm -r ./sqlite-autoconf-${SQLITE_VERSION}/
 tar xzf sqlite-autoconf-${SQLITE_VERSION}.tar.gz
-cd sqlite-autoconf-${SQLITE_VERSION}/
 
-cp -r ../sqlite-monitor/monitor/ ./
-cd monitor/
+echo ">>> SETUP MONITOR..."
+cd sqlite-autoconf-${SQLITE_VERSION}/
+cp -r ../monitor/ ./
+cd ./monitor/
+cp ./empty.sqlite ../
 ./install.sh
 
+echo ">>> TRY TO BUILD SQLITE WITH MONITOR..."
 cd ..
 ./configure
 make
@@ -17,5 +26,11 @@ then
         echo "Error: some problem with build sqlite ${SQLITE_VERSION}."
         exit
 fi
+
+echo ">>> TEST MONITOR..."
+cp -r ../monitor/ ./
+./sqlite3 ./empty.sqlite "SELECT * from  work_report;"
+cat /tmp/sqlite.log
+rm /tmp/sqlite.log
 
 #rm -rf ./sqlite-autoconf-${SQLITE_VERSION}*
